bccf93cd1e481270c7fe5963c8e1c29023cc3279
Xin Vance <xinpengfei520@gmail.com>
2022-07-07 13:00:15 +0800

    [feature][帮助]: 增加帮助页面html

---
+++ js/renew_assist_ios.js
@@ -0,0 +1,255 @@
+/*
+ * @Author: Vance
+ * @Date: 2022-07-07
+ */
+const contentList = [
+    {
+      title: '1.1 忘记登录密码怎么办？',
+      content: [
+        '<b class="ltittle">1.1 忘记登录密码怎么办？</b>',
+        '如果您忘记登录密码，请按以下步骤操作：',
+       '<ul  class="pl3rem"> <li>在登录界面选择「找回密码」</li> <li>输入绑定的手机号，获取验证码</li> <li>重置您的登录密码</li> <li><b>注意：如果手机号已不再使用，无法接收验证码，可通过【我的 ➝ 设置 ➝ 意见反馈】咨询客服解决</b></li></ul>',
+        
+      ]
+    },
+    {
+      title: '1.2 忘记手势密码怎么办？',
+      content: [
+        '<b class="ltittle">1.2 忘记手势密码怎么办？</b>',
+        '如果您忘记手势密码，请按以下步骤操作：',
+       '<ul  class="pl3rem"> <li>在绘制手势密码界面点击“忘记密码”</li><div class="tac"> <img src="./img/shoushi1.jpeg" class="wrap-pic adjust"></img></div><li>选择「手机登录」或者「微信登录」，重新登录</li> <li>或者点击最下方「退出登录」，重新选择登录方式</li>  <div class="tac"> <img src="./img/shoushi2.jpeg" class="wrap-pic adjust"></img></div>   <div class="tac"> <img src="./img/shoushi3.jpeg" class="wrap-pic adjust"></img></div><li><b>注意：账号退出，重新登录后，手势密码会失效，需重新设置</b></li></ul>',
+        
+      ]
+    },
+    {
+        title: '1.3 记呀支持哪些登录方式？',
+        content: [
+          '<b class="ltittle">1.3 记呀支持哪些登录方式？</b>',
+          '目前记呀支持手机号、微信登录方式；苹果手机，还可以通过Apple账号登录',
+       
+    
+        ]
+      },
+    {
+        title: '1.4 如何提升自己的账号安全性？',
+        content: [
+            '<b class="ltittle">1.4 如何提升自己的账号安全性？</b>',
+            '<ul  class="pl3rem"> <li>为保护您的账户安全，在收到记呀短信验证码时，请勿转发给他人，谨防账号被盗</li> <li>为保障您的数据安全，当您以游客身份试用时，请及时登录，防止数据丢失</li> </ul>',
+    
+        ]
+      },
+      {
+        title: '1.5 如何退出登录？',
+        content: [
+          '<b class="ltittle">1.5 如何退出登录？</b>',
+         '<ul  class="pl3rem"> <li>点击【我的 ➝ 设置 ➝ 账号设置 ➝ 退出登录】可退出登录</li> </ul>',
+         
+    
+        ]
+      },
+      {
+        title: '1.6 如何换账号登录？',
+        content: [
+          '<b class="ltittle">1.6 如何换账号登录？</b>',
+         '<ul  class="pl3rem"> <li>首先，点击【我的 ➝ 设置 ➝ 账号设置 ➝ 退出登录】退出登录</li> <li>然后，点击登录页下方「更多登录方式」，可切换其他账号</li></ul>',
+         
+    
+        ]
+      },
+      {
+        title: '1.7 可以在多个设备上登录同一账号吗？',
+        content: [
+            '<b class="ltittle">1.7 可以在多个设备上登录同一账号吗？</b>',
+            '<ul  class="pl3rem"> <li>可以。同一账号在不同设备上可同时登录，记账数据会自动同步</li> </ul>',
+    
+        ]
+      },
+      {
+        title: '1.8 手机号/微信号绑定问题',
+        content: [
+          '<b class="ltittle">1.8 手机号/微信号绑定问题</b>',
+          '<ul  class="pl3rem"> <li>为提高安全性，微信登录时建议您绑定手机号</li>  <li>若想绑定的手机号提示“手机号已注册”，建议您核实该手机号是否还在使用</li><li>若是您本人使用，可尝试：<b>使用该手机号登录-注销该账号-切换回微信登录，重新绑定</b></li> <li>若非您本人使用，可通过【我的 ➝ 设置 ➝ 意见反馈】咨询客服解决</li></ul>',
+    
+        ]
+      },
+      {
+        title: '1.9 手机号无法接收验证码，怎么办？',
+        content: [
+          '<b class="ltittle">1.9 手机号无法接收验证码，怎么办？</b>',
+         '<ul  class="pl3rem"> <li>确认手机是否处于停机、欠费状态，如手机还会使用，建议充值手机话费后操作</li> <li>清理手机收件箱，释放手机内存</li> <li>手机设备进行关机重启，保证短信接收效果</li> <li>如果是因为  <b>旧手机号不再使用，收不到验证码，</b>可通过【我的 ➝ 设置 ➝ 意见反馈】咨询客服解决</li></ul>',
+        ]
+      },
+      {
+        title: '1.10 如何注销账号？',
+        content: [
+          '<b class="ltittle">1.10 如何注销账户？</b>',
+         '<ul  class="pl3rem"> <li>点击【我的】页头像，进入个人信息页，点击最下方「注销账号」即可</li> <li><b>注意：注销后所有信息将被清空且无法找回，建议谨慎操作</b></li>  </ul>',
+         '    <div class="tac"> <img src="./img/zhuxiao.png" class="wrap-pic adjust"></img></div>',  
+
+        ]
+      },
+      {
+        title: '2.1 如何添加更多记账类别？',
+        content: [
+          '<b class="ltittle">2.1 如何添加更多记账类别？</b>',
+         '<ul  class="pl3rem"> <li>当系统提供的记账类别不够用时，可通过【添加类别】功能添加更多记账类别</li> <li>点击【我的 ➝ 设置 ➝ 类别设置 ➝ 添加类别】，即可使用</li> <li>普通用户可添加20个自定义类别，VIP用户可添加50个自定义类别</li><li>长按类别右侧按钮，可拖动调整顺序</li><div class="tac"> <img src="./img/shanchuleibie1.jpeg" class="wrap-pic adjust"></img></div><li>点击类别左侧删除按钮，可删除类别</li><div class="tac"> <img src="./img/shanchuleibie2.jpeg" class="wrap-pic adjust"></img></div><li><b>注意：删除某个类别时，需要先删除该类别下的全部数据，建议谨慎操作</b></li></ul>',
+      
+    
+        ]
+      },
+      {
+        title: '2.2 如何设置预算？',
+        content: [
+          '<b class="ltittle">2.2 如何设置预算？</b>',
+         '<ul  class="pl3rem"> <li>合理制定预算，可帮助您进一步实现理性消费</li> <li>点击【明细 → 预算】或点击【我的 → X月总预算】进入预算功能，在此可以编辑当月总预算。还可进一步针对具体的类别设置分类预算，点击下方「添加分类预算」即可</li><li>升级为VIP会员后可使用分类预算功能 </li> <li>设置预算后，【明细】页左上方的智能助手，会每天提醒预算结余情况</li></ul>',
+        ]
+      },
+      {
+        title: '2.3 如何创建多个账本？',
+        content: [
+          '<b class="ltittle">2.3 如何创建多个账本？</b>',
+         '<ul  class="pl3rem"> <li>当您需要多个账本独立管理您的记账数据时（例如：生意账本、报销账本），可以使用分账本功能</li> <li>请将记呀升级到4.0及以上版本，点击【明细 ➝ 更多 ➝ 账本管理 ➝ 创建账本】，即可使用该功能</li> <li>记呀分账本完全独立于主账本，账本之间数据不互通，为创建的分账本添加桌面入口，一键打开分账本，使用更便捷</li><li>升级为VIP会员后可使用该功能</li></ul>',
+    
+        ]
+      },
+      {
+        title: '2.4 如何设置解锁密码？',
+        content: [
+          '<b class="ltittle">2.4 如何设置解锁密码？</b>',
+         '<ul  class="pl3rem"> <li>为保护您的个人隐私，可为软件设置解锁密码</li> <li>通过【我的 ➝ 设置 ➝ 解锁密码/手势密码】，即可设置手势密码/面容解锁/指纹解锁</li> <li>升级为VIP会员后可使用该功能</li></ul>',
+    
+        ]
+      },
+      {
+        title: '2.5 如何使用自动定时记账？',
+        content: [
+          '<b class="ltittle">2.5 如何使用自动定时记账？</b>',
+    
+         '<ul  class="pl3rem"> <li>如果您需要记录定期重复发生的收入/支出，如：房贷、房租、工资等，可通过定时记账功能自动记录</li> <li>点击【我的 ➝ 设置 ➝ 定时记账 ➝ 添加定时记账】，即可使用。当前自动记账重复的周期有：每天、每周、每月、永不</li>  <li>升级为VIP会员后可使用该功能</li></ul>',
+    
+        ]
+      },
+      {
+        title: '2.6 如何自定义记账周期/设置每月开始日？',
+        content: [
+            '<b class="ltittle">2.6 如何自定义记账周期/设置每月开始日？</b>',
+         '<ul  class="pl3rem"> <li>若您想自定义每个月的记账周期，可更改每月开始日</li> <li>点击【我的 ➝ 设置 ➝ 每月开始于】进行设置</li> <li>设置后，【明细】页上方会提示当前的月周期，可点击提示区域修改设置</li><li>升级为VIP会员后可使用该功能</li> </ul>',
+         
+    
+        ]
+      },
+      {
+        title: '2.7 如何更换主题/皮肤？  ',
+        content: [
+          '<b class="ltittle">2.7 如何更换主题/皮肤？</b>',
+    
+         '<ul  class="pl3rem"><li>点击【我的 ➝ 设置 ➝ 个性装扮】即可设置</li><li>升级为VIP会员后可使用该功能</li></ul>',
+         
+    
+        ]
+      },
+      {
+        title: '3.1 如何快速修改一笔账？',
+        content: [
+          '<b class="ltittle">3.1 如何快速修改一笔账？</b>',
+         '<ul  class="pl3rem"> <li>请确认【我的 ➝ 设置 ➝ 明细详情】处的按钮置于关闭状态</li> <div class="tac"> <img src="./img/kuaijie1.jpeg" class="wrap-pic adjust"></img></div>  <li>轻点【明细】页每一笔账单的类别图标、备注、金额，可修改相应内容</li> <div class="tac"> <img src="./img/kuaijie2.jpeg" class="wrap-pic adjust"></img></div>            <div class="tac"> <img src="./img/kuaijie3.jpeg" class="wrap-pic adjust"></img></div></ul>',
+            
+
+        ]
+      },
+      {
+        title: '3.2 如何指定日期记账？/如何补记账？',
+        content: [
+          '<b class="ltittle">3.2 如何指定日期记账？/如何补记账？</b>',
+         '<ul  class="pl3rem">   <div class="pb00"><b >方式一：</b> </div ><li>记账时，点击键盘上的「今天」 </li>  <div class="tac"> <img src="./img/buji1.jpeg" class="wrap-pic adjust"></img></div> <li>弹出时间选项后，选择日期即可</li> <div class="tac"> <img src="./img/buji2.jpeg" class="wrap-pic adjust"></img></div><div class="pb00"><b>方式二：</b></div><li>升级为会员，使用日历记账。点击【明细】页右上方日历图标<div class="tac"> <img src="./img/buji3.png" class="wrap-pic adjust"></img></div></li><li>选中某天后，轻触下方➕号 ，即可快速补记账</li> <div class="tac"> <img src="./img/buji4.jpeg" class="wrap-pic adjust"></img></div>     </ul>',
+ 
+
+        ]
+      },
+      {
+        title: '3.3 如何修改记账日期？',
+        content: [
+          '<b class="ltittle">3.3 如何修改记账日期？</b>',
+         '<ul  class="pl3rem"><div class="pb00"><b>方式一：</b></div><li>【设置-明细详情】按钮关闭</li><div class="tac"> <img src="./img/riqi1.jpeg" class="wrap-pic adjust"></img></div><li>轻点【明细】页记账明细的金额，点击键盘上的日期</li> <div class="tac"> <img src="./img/riqi2.jpeg" class="wrap-pic adjust"></img></div> <li> 重新选择日期即可</li> <div class="tac"> <img src="./img/riqi3.jpeg" class="wrap-pic adjust"></img></div><div class="pb00"><b>方式二：</b></div><li>【设置-明细详情】按钮打开<div class="tac"> <img src="./img/riqi4.jpeg" class="wrap-pic adjust"></img></div><li> 点击某笔账单，进入详情页，点击详情页下方「编辑」 </li> <div class="tac"> <img src="./img/riqi5.jpeg" class="wrap-pic adjust"></img></div> <li> 点击键盘上的日期</li> <div class="tac"> <img src="./img/riqi6.jpeg" class="wrap-pic adjust"></img></div> <li> 重新选择日期即可</li> <div class="tac"> <img src="./img/riqi7.jpeg" class="wrap-pic adjust"></img></div></ul>',
+    
+
+        ]
+      },
+      {
+        title: '3.4 如何删除一笔账？',
+        content: [
+          '<b class="ltittle">3.4 如何删除一笔账？</b>',
+         '<ul  class="pl3rem"> <div class="pb00"><b>方式一：</b></div><li>轻触记账明细向左划，出现删除按钮，点击按钮即可删除</li> <div class="tac"> <img src="./img/shanchu1.jpeg" class="wrap-pic adjust"></img></div> <div class="pb00"><b>方式二：</b></div><li>【设置-明细详情】按钮打开<div class="tac"> <img src="./img/riqi4.jpeg" class="wrap-pic adjust"></img></div><li> 点击某笔账单，进入详情页，点击详情页下方「删除」即可 </li> <div class="tac"> <img src="./img/shanchu3.jpeg" class="wrap-pic adjust"></img></div><li><b>注意：删除后的数据无法找回，建议谨慎操作</b></li></ul>',
+        ]
+      },
+      {
+        title: '4.1 如何使用搜索功能？',
+        content: [
+          '<b class="ltittle">4.1 如何使用搜索功能？</b>',
+         '<ul  class="pl3rem"><li>点击【明细】页右上方搜索标识</li><div class="tac"> <img src="./img/sousuo1.png" class="wrap-pic adjust"></img></div> <li>可根据记账类别、备注进行搜索</li><div class="tac"> <img src="./img/sousuo2.png" class="wrap-pic adjust"></img></div><div class="tac"> <img src="./img/sousuo3.jpeg" class="wrap-pic adjust"></img></div><li>升级为VIP会员后可使用该功能</li>  </ul>',
+
+        ]
+      },
+      {
+        title: '4.2 如何查看指定日期的记账信息？',
+        content: [
+          '<b class="ltittle">4.2 如何查看指定日期的记账信息？</b>',
+         '<ul  class="pl3rem"> <li>点击【明细】页右上方日历标识</li>         <div class="tac"> <img src="./img/rili1.png" class="wrap-pic adjust"></img></div> <li>选择日期后，会显示当天所有账单</li>         <div class="tac"> <img src="./img/rili2.jpeg" class="wrap-pic adjust"></img></div><li>升级为VIP会员后可使用该功能</li> </ul>',
+        ]
+      },
+      {
+        title: '4.3 如何隐藏首页顶部的收入/支出总金额？',
+        content: [
+          '<b class="ltittle">4.3 如何隐藏首页顶部的收入/支出总金额？</b>',
+         '<ul  class="pl3rem"> <li>轻点【明细】页右上方「眼睛」的标识可隐藏收入、支出总金额</li>    <div class="tac"> <img src="./img/yincang1.png" class="wrap-pic adjust"></img></div>  <div class="tac"> <img src="./img/yincang2.png" class="wrap-pic adjust"></img></div><li>升级为VIP会员后可使用该功能</li></ul>',
+      
+        ]
+      },
+      {
+        title: '4.4 如何使用饼图查看收支统计？',
+        content: [
+          '<b class="ltittle">4.4 如何使用饼图查看收支统计？</b>',
+         '<ul  class="pl3rem"> <li>轻点【图表】页右上方「饼图」标识</li>         <div class="tac"> <img src="./img/bingtu1.jpeg" class="wrap-pic adjust"></img></div>  <li>进入饼图统计页，查看饼图统计结果</li>         <div class="tac"> <img src="./img/bingtu2.jpeg" class="wrap-pic adjust"></img></div> <li>升级为VIP会员后可使用该功能</li></ul>',
+        ]
+      },
+      {
+        title: '4.5 如何导出记账数据？',
+        content: [
+          '<b class="ltittle">4.5 如何导出记账数据？</b>',
+          '<ul  class="pl3rem"><li>点击【我的 ➝ 设置 ➝ 导出数据】</li><div class="tac"> <img src="./img/daochu1.jpeg" class="wrap-pic adjust"></img></div><li>可自定义导出账单的起止时间</li><div class="tac"> <img src="./img/daochu2.jpeg" class="wrap-pic adjust"></img></div> <li>导出的文件为csv格式，您可以：</li> <ul  class="pl3rem"> <li>使用手机上已安装的浏览器或其他软件打开查看</li><li> 还可以分享至第三方后，使用电脑打开查看</li> </ul> <div class="tac"> <img src="./img/daochu3.jpeg" class="wrap-pic adjust"></img></div><li><b>注意：如果导出的excel表日期列显示为“######”，此为日期列宽度过小所致，拉宽日历列即可</b></li><li>升级为VIP会员后可使用该功能</li></ul>',
+        ]
+      },
+      {
+        title: '4.6 如何使用资产管家？',
+        content: [
+          '<b class="ltittle">4.6 如何使用资产管家？</b>',
+          '<ul  class="pl3rem"><li>由【明细 ➝ 资产管家】进入</li><div class="tac"> <img src="./img/zichan1.png" class="wrap-pic adjust"></img></div><li>点击下方「添加资产」按钮，添加各项资产</li> <div class="tac"> <img src="./img/zichan2.jpeg" class="wrap-pic adjust"></img></div><li>【资产】页可以查看各项资产 </li> <div class="tac"> <img src="./img/zichan3.jpeg" class="wrap-pic adjust"></img></div> <li>【图表】页可以查看资产的排行榜及各资产在总资产中的占比 </li> <div class="tac"> <img src="./img/zichan4.jpeg" class="wrap-pic adjust"></img></div><li>只需定期记录资产数据，即可轻松掌握财务状况</li> </ul>',
+
+        ]
+      },
+      {
+        title: '5.1 购买会员后，如何查看会员权益是否生效？',
+        content: [
+          '<b class="ltittle">5.1 购买会员后，如何查看会员权益是否生效？</b>',
+         '<ul  class="pl3rem"> <li>购买会员后，【我的VIP】页显示的权益会全部解锁，可去对应位置查看该权益是否可用。此外，【我的】页会有VIP标识、VIP剩余天数</li> <li>如果支付成功已扣款，但是VIP未生效，可尝试如下方法：</li> <ul class="pl3rem"><li>点击【我的VIP ➝ 恢复购买】尝试恢复（尝试恢复为刷新流程，不会重复扣款）。或关掉APP进程，重新启动记呀，进入我的VIP页查看。如果仍未生效，可通过【我的 ➝ 设置 ➝ 意见反馈】咨询客服解决</li></ul></ul>',
+
+        ]
+      },
+       {
+        title: '5.2 其他会员问题',
+        content: [
+          '<b class="ltittle">5.2 其他会员问题</b>',
+
+'<ul class="pl3rem"><li class="wrap-title">VIP会员的有效期如何查看？</li><div class="wrap-content">有效期按照所购买VIP时长具体计算，例如2021年11月10日购买了一年VIP，则VIP会员有效期到2022年11月10日；开通会员后，可以前往我的VIP页查看会员有效期</div><li class="wrap-title">购买了会员后所有功能都是免费的吗？</li><div class="wrap-content">购买了VIP会员之后，我的VIP页列举的会员权益，您都可以在会员有效期内免费使用</div><li class="wrap-title">正式版VIP和体验版VIP有什么区别？</li><ul class="wrap-content"><li class="lh2">除免广告、分账本、双倍积分以外，体验版VIP享有其它正式版VIP的所有权益</li><li class="lh2"> 如果同时有正式版VIP和体验版VIP，优先使用正式版VIP天数；正式版VIP到期且不再自动续费后，才会使用体验版VIP</li><li class="lh2"> 体验版VIP不影响正式版VIP自动续费</li></ul><li class="wrap-title">记呀VIP是否支持第三方渠道购买？</li><ul class="wrap-content"><li>目前仅支持在记呀App内购买VIP会员服务</li><li>记呀从未授权任何第三方单位或个人销售、转让记呀VIP会员资格</li><li>通过第三方渠道购买VIP会员有账号被盗、财务损失等风险</li><li>任何非官方渠道购买的VIP会员，均存在被冻结资格的风险</li></ul><li class="wrap-title">购买之后可以退订/更换会员吗？</li><div class="wrap-content"><div class="lh2">已购会员属于虚拟产品，购买之前请仔细阅读相关服务条款及购买内容，一经购买/开通，立即生效，不支持退款</div></div><li class="wrap-title">一个账号购买会员之后可以转移到其他账号吗？</li><div class="wrap-content">会员购买之后只能用购买时所使用的账号，不可以转移到其他账号</div><li class="wrap-title">会员会被冻结吗？</li><div class="wrap-content">会被冻结。如发现您存在违规操作（包括但不限于虚拟交易、恶意退款、商业牟利）等行为，我们有权对您的会员卡账户进行冻结，并有权取消您的会员权益，有权立即清除账户中所有的特权行为</div><li class="wrap-title">苹果用户如何取消自动订阅？</li><div class="wrap-content">过手机【设置】，进入【iTunes Store 与 App Store】，点击 【Apple ID】，选择【查看Apple ID】，进入【账户设置】，点击【订阅】，选择记呀取消订阅即可。取消订阅成功后，苹果手机订阅管理最下方会显示「选择选项以重新订阅」字样提示</div></ul>'
+        ]
+      },
+      {
+        title: '6.1 如何去除广告？',
+        content: [
+          '<b class="ltittle">6.1 如何去除广告？</b>',
+          '<ul  class="pl3rem"> <li>广告资助记呀持续提供更好的服务，我们会尽最大努力确保广告不会影响您的记账体验</li><li>如果您不想看到广告，也可以升级为VIP会员</li><ul/>'
+
+        ]
+      },
+  ]
+  
